52a59ec6cf750f1492d4ded2509d20a65cb66d2b
MHPontes <mateushspontes@gmail.com>
2024-03-14 15:08:24 -0300

    Criando classe ConverterExtensionFunction responsavel por converter as classes de Put e Post para uma CustomerModel, e removendo essas chamadas de nossa Service, para que permaneca sempre somente a CustomerModel

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,7 +1,5 @@
 package com.mercadolivro.service
 
-import com.mercadolivro.controller.request.PostCustomerRequest
-import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
@@ -26,18 +24,20 @@ class CustomerService {
         return customers.filter { it.id == id }.first()
     }
 
-    fun create(customer: PostCustomerRequest) {
+    fun create(customer: CustomerModel) {
         val id = if (customers.isEmpty()) {
             1
         } else {
-            customers.last().id.toInt() + 1
+            customers.last().id!!.toInt() + 1
         }.toString()
+
+        customer.id = id
 
         customers.add(CustomerModel(id, customer.name, customer.email))
     }
 
-    fun update(id: String, customer: PutCustomerRequest) {
-        customers.filter { it.id == id }.first().let {
+    fun update(customer: CustomerModel) {
+        customers.filter { it.id == customer.id }.first().let {
             it.name = customer.name
             it.email = customer.email
         }
