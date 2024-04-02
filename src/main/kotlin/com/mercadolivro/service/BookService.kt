090413b7c2e37090b318fe97061c74f009f5a680
MHPontes <mateushspontes@gmail.com>
2024-04-02 09:16:16 -0300

    Adicionando classe NotFoundException em nosso package exception, para tratar erros NotFound

--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.service
 
 import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.exception.NotFoundException
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.BookRepository
@@ -27,7 +28,7 @@ class BookService(
     }
 
     fun findById(id: Int): BookModel {
-        return bookRepository.findById(id).orElseThrow{Exception("Book not found")} //validar somente teste
+        return bookRepository.findById(id).orElseThrow{NotFoundException("Book [${id}] not exists", "ML-0001")} //validar somente teste
     }
 
     fun delete(id: Int) {
