92bb9c83bc6991a7ed7ba5d4a3fd872eb788ef38
MHPontes <mateushspontes@gmail.com>
2024-04-29 11:11:07 -0300

    Adicionando anotation para que nossos eventos sejam assincronos .

--- src/main/kotlin/com/mercadolivro/events/listeners/GenerateNfeListener.kt
+++ src/main/kotlin/com/mercadolivro/events/listeners/GenerateNfeListener.kt
@@ -3,6 +3,7 @@ package com.mercadolivro.events.listeners
 import com.mercadolivro.events.PurchaseEvent
 import com.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -11,6 +12,7 @@ class GenerateNfeListener(           //Ira gerar as NFEs
     private val purchaseService: PurchaseService
 ) {
 
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent){
         val nfe = UUID.randomUUID().toString()   //gerando nfes
