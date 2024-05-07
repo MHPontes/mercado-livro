5542ab627dd37a1d97c924c3c4ee27ba9bad6e7b
MHPontes <mateushspontes@gmail.com>
2024-05-07 11:02:32 -0300

    Adicionando validação para que compra seja permitida somente para livros ativos e adicionando novo erro de negocio para esta fluxo.

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -5,5 +5,6 @@ enum class Errors(val code: String, val message: String) {
     ML001("ML-001", "Invalid request"),
     ML101("ML-101", "Book [%s] not exists"),
     ML102("ML-102", "Cannot update book with status [%s]"),
+    ML103("ML-103", "Cannot purchase book [%s] because status is not ATIVO"),
     ML201("ML-201", "Customer [%s] not exists")
 }
--- src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
+++ src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
@@ -1,7 +1,11 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.enums.Errors
 import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.exception.BadRequestException
 import com.mercadolivro.model.PurchaseModel
+import com.mercadolivro.repository.BookRepository
 import com.mercadolivro.repository.PurchaseRepository
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
@@ -9,14 +13,30 @@ import org.springframework.stereotype.Service
 @Service
 class PurchaseService(
     private val purchaseRepository: PurchaseRepository,
+    private val bookRepository: BookRepository,
     private val applicationEventPublisher: ApplicationEventPublisher   //Injentando para disparar eventos
 ) {
 
+    // =======================
+    // AULA - 74 - DESAFIO DE IMPLEMENTAÇÃO - Fazer com que não seja possivel realizar a compra de um livro que não esteja disponivel.
+    // =======================
     fun create(purchaseModel: PurchaseModel) {
-        purchaseRepository.save(purchaseModel)
+        val bookIds = purchaseModel.books.map { it.id }
 
-        applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel))
+        // Busca os livros com base nos IDs
+        val books = bookRepository.findAllById(bookIds)
+
+        // Encontra os livros inativos e armazena seus IDs
+        val inactiveBookIds = books.filter { it.status != BookStatus.ATIVO }.map { it.id }
+
+        if (inactiveBookIds.isNotEmpty()) {
+            throw BadRequestException(Errors.ML103.message.format(inactiveBookIds), Errors.ML103.code)
+        } else {
+            purchaseRepository.save(purchaseModel)
+            applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel))
+        }
     }
+
 
     fun update(purchaseModel: PurchaseModel) {
         purchaseRepository.save(purchaseModel)
