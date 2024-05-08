ddf8ed70757caa8adf70af93ab315adc0805cb4f
MHPontes <mateushspontes@gmail.com>
2024-05-08 17:13:29 -0300

    Iniciando primeiras configuracoes de seguranca

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,10 +2,34 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    private val PUBLIC_POST_MATCHERS = arrayOf(
+        "customers"
+    )
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { requests ->
+            requests.requestMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+            requests.anyRequest().authenticated()
+        }
+        http.httpBasic {} // Configuração do HttpBasic
+        http.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        http.csrf { it.disable() } // Desabilita CSRF
+        http.cors { it.disable() }
+        return http.build()
+    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
